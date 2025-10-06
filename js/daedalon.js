2b972a5bfb53bc7ae36d12764a45e1f55d45e878
Xander Berger <xandergberger@gmail.com>
2025-10-06 13:56:32 -0400

    Little JS Fixes

--- js/daedalon.js
+++ js/daedalon.js
@@ -1,9 +1,7 @@
 "use strict";
 
-$(document).ready( () =>
-    $( function() {
-        $("#accordion-1").accordion({collapsible: true, active: false});
-        $("#accordion-2").accordion({collapsible: true, active: false});
-        $("#accordion-3").accordion({collapsible: true, active: false});
-    })
-);
+$(document).ready( () => {
+    $("#accordion-1").accordion({collapsible: true, active: false})
+    $("#accordion-2").accordion({collapsible: true, active: false})
+    $("#accordion-3").accordion({collapsible: true, active: false})
+});
