3299b31d557ddf20e41c881ce60be38d5e06fa05
Xander Berger <xandergberger@gmail.com>
2025-10-06 13:50:58 -0400

    Proper Code Changes

--- js/daedalon.js
+++ js/daedalon.js
@@ -1,7 +1,9 @@
 "use strict";
 
-$( function() {
-    $("#accordion-1").accordion({collapsible: true, active: false});
-    $("#accordion-2").accordion({collapsible: true, active: false});
-    $("#accordion-3").accordion({collapsible: true, active: false});
-});
+$(document).ready( () =>
+    $( function() {
+        $("#accordion-1").accordion({collapsible: true, active: false});
+        $("#accordion-2").accordion({collapsible: true, active: false});
+        $("#accordion-3").accordion({collapsible: true, active: false});
+    })
+);
